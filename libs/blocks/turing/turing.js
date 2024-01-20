2b287b2bd2b6bd755022dd3fc7857c039bea7d87
Aishwarya Mathuria <mathuria@adobe.com>
2024-01-20 22:24:15 +0530

    Update turing.js

--- libs/blocks/turing/turing.js
+++ libs/blocks/turing/turing.js
@@ -1,5 +1,17 @@
 export default function init(el) {
 el.innerHTML = `<div class="carousel-container"><div class="carousel_items"><div class="carousel_item item1"><p class="carousel_text">Image 1</p></div><div class="carousel_item item2"><p class="carousel_text">Image 2</p></div><div class="carousel_item item3"><p class="carousel_text">Image 3</p></div><div class="carousel_item item4"><p class="carousel_text">Image 4</p></div><div class="carousel_item item5"><p class="carousel_text">Image 5</p></div></div></div>`;
+imgList = ['https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+          'https://images.unsplash.com/photo-1501862700950-18382cd41497?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=519&q=80',
+          'https://images.unsplash.com/photo-1536697246787-1f7ae568d89a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNwYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+          'https://images.unsplash.com/photo-1620712943543-bcc4688e7485?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8QUl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+          'https://images.unsplash.com/photo-1673901736622-c3f06b08511f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80',
+          ]
+const x = document.querySelector(".carousel_items");
+imgList.forEach((img, i) => {
+  x.innerHTML = x.innerHTML + `<div class="carousel_item item4"> <p class="carousel_text">Image 4</p> </div>`;
+  const tmp = x.querySelector('div:last-child');
+  tmp.backgroundImage = `url(${imgList[i]})`;
+});
 const carouselItems = document.querySelectorAll(".carousel_item");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
