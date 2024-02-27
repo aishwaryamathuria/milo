f0ff5cbab3ea4fecbd04dc02c31275c2619b3df7
mathuria <mathuria@adobe.com>
2024-02-27 23:35:10 +0530

    test

--- libs/blocks/marquee/marquee.js
+++ libs/blocks/marquee/marquee.js
@@ -143,7 +143,7 @@ export default async function init(el) {
     <line stroke="#fff" x1="12" y1="16" x2="12" y2="8"/>
   </svg>`;
   el.querySelector('.foreground .asset.image').innerHTML += `<label id="file-input-label" for="file-input">${inputSvg} Select a File</label><input type='file' class='upload-file' id="file-input" name="file-input" />`;
-  el.querySelector('.foreground .asset.image #file-input-label').className = el.querySelector('.action-area #file-input-label').className + ' blue ' + el.querySelector('.action-area .con-button').className;
+  el.querySelector('.foreground .asset.image #file-input-label').className = el.querySelector('.foreground .asset.image #file-input-label').className + ' con-button blue button-xl button-justified-mobile';
   el.querySelector('.foreground .asset.image .upload-file').addEventListener('change', (e) => {
     var file = e.target.files[0];
     if (!file) {
