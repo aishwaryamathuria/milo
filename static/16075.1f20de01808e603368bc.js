41c8000dd86edcf357699e833686e5fe3fd012e5
Aishwarya Mathuria <mathuria@adobe.com>
2024-03-07 13:41:51 +0530

    Create 16075.1f20de01808e603368bc.js

---
+++ static/16075.1f20de01808e603368bc.js
@@ -0,0 +1,18 @@
+"use strict";(self.webpackChunk_hz_quick_actions_standalone_webapp=self.webpackChunk_hz_quick_actions_standalone_webapp||[]).push([[16075],{516075:(a,e,t)=>{t.r(e);var h=t(903512),i=t(400779),l=t(857534);class s extends i.A{render(){return(0,l._)(h.dy),(({width:a=24,height:e=24,hidden:t=!1,title:h="Export"}={})=>l.A`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    height=${e}
+    viewBox="0 0 36 36"
+    width=${a}
+    aria-hidden=${t?"true":"false"}
+    role="img"
+    fill="currentColor"
+    aria-label=${h}
+  >
+    <path
+      d="M25 26h-2a1 1 0 0 0-1 1v3H6V6h16v3a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v30a1 1 0 0 0 1 1h22a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1Z"
+    />
+    <path
+      d="M35.856 17.649 29.332 10.2a.787.787 0 0 0-.527-.2.8.8 0 0 0-.8.8V16H17a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h11v5.2a.8.8 0 0 0 .8.8.787.787 0 0 0 .527-.2l6.524-7.445a.5.5 0 0 0 0-.7Z"
+    />
+  </svg>`)({hidden:!this.label,title:this.label})}}(0,t(589415).N)("sp-icon-export",s)}}]);
+//# sourceMappingURL=16075.1f20de01808e603368bc.js.map
