fca869a2a723c58876abe7d422624e3b2c90a149
Aishwarya Mathuria <mathuria@adobe.com>
2024-03-08 17:17:42 +0530

    Update marquee.js

--- libs/blocks/marquee/marquee.js
+++ libs/blocks/marquee/marquee.js
@@ -206,10 +206,9 @@ export function addtoIframe(el, fetchAndConvert=true, localBase64String='') {
       if (!ccEverywhere) {
         let env = 'preprod';
         ccEverywhere = await window.CCEverywhere.initialize({
-          clientId: '9c87596521804dc1a52cd8508b585146',
+          clientId: 'bdc3513d3eda45c885ba846815f2a951',
           appName: 'ExpressEmbedDemoApplication',
           platformCategory: "web",
-          redirectUri: "https://demo-stage.expressembed.com/"
         }, {
           loginMode: 'delayed',
           env,
@@ -255,7 +254,7 @@ export function addtoIframe(el, fetchAndConvert=true, localBase64String='') {
       if (!ccEverywhere) {
         let env = 'preprod';
         ccEverywhere = await window.CCEverywhere.initialize({
-          clientId: '9c87596521804dc1a52cd8508b585146',
+          clientId: 'bdc3513d3eda45c885ba846815f2a951',
           appName: 'ExpressEmbedDemoApplication',
         }, {
           loginMode: 'delayed',
