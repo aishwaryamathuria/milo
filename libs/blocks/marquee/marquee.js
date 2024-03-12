e1f2e75ef1a4533cd572dca49e35f13520069edf
Aishwarya Mathuria <mathuria@adobe.com>
2024-03-12 14:36:31 +0530

    Update marquee.js

--- libs/blocks/marquee/marquee.js
+++ libs/blocks/marquee/marquee.js
@@ -135,8 +135,10 @@ export default async function init(el) {
     await loadMnemonicList(foreground);
   }
 
-  el.querySelector('.asset').innerHTML = '';
-  addtoIframe(el);
+  el.querySelector('.asset').addEventListener((e) => {
+    el.querySelector('.asset').innerHTML = '';
+    addtoIframe(el);
+  })
 
   const inputSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
     <circle cx="12" cy="12" r="10" stroke="#fff"/>
