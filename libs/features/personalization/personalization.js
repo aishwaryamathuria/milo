e9c42cd40de72fc97cd44cd71c1eaf2d0b093974
Aishwarya Mathuria <mathuria@adobe.com>
2024-11-11 12:18:23 +0530

    Update personalization.js

--- libs/features/personalization/personalization.js
+++ libs/features/personalization/personalization.js
@@ -465,7 +465,7 @@ export const updateFragDataProps = (a, inline, sections, fragment) => {
 
 export function handleCommands(commands, rootEl = document, forceInline = false) {
   if (window.alloy_all.data._adobe_corpnew.digitalData.page.pageInfo.pageName != window.alloy_all.data._adobe_corpnew.digitalData.previousPage.pageInfo.pageName) {
-    if (window.alloy_all.data._adobe_corpnew.digitalData.previousPage.pageInfo.pageName.contains('remove-background')) {
+    if (window.alloy_all.data._adobe_corpnew.digitalData.previousPage.pageInfo.pageName.includes('remove-background')) {
       commands = [
           {
               "action": "replace",
@@ -477,7 +477,7 @@ export function handleCommands(commands, rootEl = document, forceInline = false)
               "targetManifestId": false
           }
       ];
-    } else if (window.alloy_all.data._adobe_corpnew.digitalData.previousPage.pageInfo.pageName.contains('generative')) {
+    } else if (window.alloy_all.data._adobe_corpnew.digitalData.previousPage.pageInfo.pageName.includes('generative')) {
       commands = [
           {
               "action": "replace",
