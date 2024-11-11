7fa89fd78b01c330e05d79c5edeaf76a3cc60478
Aishwarya Mathuria <mathuria@adobe.com>
2024-11-11 12:33:48 +0530

    Update turing.js

--- libs/blocks/turing/turing.js
+++ libs/blocks/turing/turing.js
@@ -24,6 +24,7 @@ const options = {method: 'GET', headers: {'x-api-key': 'alfred-stage'}};
 const rawImgJson = await fetch('https://cc-api-cp-stage.adobe.io/api/v2/ff_community/assets?sort=date_desc&size=10', options);
 const imgJson = await rawImgJson.json();
 const imgList = [];
+const imgTitle = [];
 imgJson._embedded.assets.forEach((i) => {
   let aw = i._embedded.artwork._links.rendition.href;
   console.log(aw)
@@ -31,11 +32,12 @@ imgJson._embedded.assets.forEach((i) => {
   aw = aw.replace("{dimension}", "width");
   aw = aw.replace("{size}", "400");
   imgList.push(aw);
+  imgTitle.push(i._embedded.title);
 });
 const x = document.querySelector(".carousel_items");
 for (let j = 0; j<=10; j++) {
   imgList.forEach((img, i) => {
-    x.innerHTML = x.innerHTML + `<div class="carousel_item item${i+1}"> <p class="carousel_text">Image ${i+1}</p> </div>`;
+    x.innerHTML = x.innerHTML + `<div class="carousel_item item${i+1}"> <p class="carousel_text">${imgTitle[i]}</p> </div>`;
     const tmp = x.querySelector('div:last-child');
     tmp.style.backgroundImage = `url("${imgList[i]}")`;
   });
@@ -43,27 +45,13 @@ for (let j = 0; j<=10; j++) {
 const carouselItems = document.querySelectorAll(".carousel_item");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
-
-// It will be used to shift the items  by incrementing its
-// value every 2 seconds and multiplying it by 100
 let i = 1;
-
-// Running the condition every 2 seconds using setInterval
 setInterval(() => {
-  // Accessing All the carousel Items
   Array.from(carouselItems).forEach((item, index) => {
-    // Checking if the value of i is less than the
-    // number of carousel items, if it is true,
-    // move all the carousel items 100% towards left
     if (i < carouselItems.length) {
       item.style.transform = `translateX(-${i * 100}%)`;
     }
   });
-
-  // If the the value of i is less than the number of
-  // Carousel items, then increment the value of i by 1
-  // Otherwise set the value of i to 0 if it becomes greater
-  // than the number of carousel items
   if (i < carouselItems.length) {
     i++;
   } else {
