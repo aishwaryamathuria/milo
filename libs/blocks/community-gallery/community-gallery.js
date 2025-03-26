a4672a2de9bff8da8f24f154e2e82ae76fb8af28
kusumadevadiga <31731487+kusumadevadiga@users.noreply.github.com>
2025-03-26 17:03:38 +0530

    Update community-gallery.js

--- libs/blocks/community-gallery/community-gallery.js
+++ libs/blocks/community-gallery/community-gallery.js
@@ -9,6 +9,12 @@ async function loadComponent() {
 }
 
 export default async function init(el) {
+   await Promise.all([
+       loadScript(`${window.location.origin}/libs/blocks/community-gallery/spectrum-base.js`, 'module'),
+       loadScript(`${window.location.origin}/libs/blocks/community-gallery/spectrum-lit-all.min.js`, 'module'),
+       loadScript(`${window.location.origin}/libs/blocks/community-gallery/spectrum-theme.js`, 'module')
+   ]);
+  
   await loadComponent();
   const c = createTag('sp-theme', { dir: 'ltr', scale: 'medium', color: 'light'});
   el.insertAdjacentElement("afterend", c);
