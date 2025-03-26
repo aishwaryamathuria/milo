23000e6609022e137fb7258ba446298659c24645
kusumadevadiga <31731487+kusumadevadiga@users.noreply.github.com>
2025-03-26 22:08:23 +0530

    Create swc-react-theme.js

---
+++ libs/blocks/community-gallery/swc-react-theme.js
@@ -0,0 +1,2 @@
+"use strict";import*as e from"react";import{createComponent as m}from"@lit/react";import{Theme as t}from"@spectrum-web-components/theme";import"@spectrum-web-components/theme/sp-theme.js";export const Theme=m({displayName:"Theme",elementClass:t,react:e,tagName:"sp-theme",events:{}});
+//# sourceMappingURL=index.js.map
